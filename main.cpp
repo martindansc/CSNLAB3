245488232574e83ff876b65386ee6db594488ebc
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2019-10-26 21:43:10 +0200

    organize code

--- main.cpp
+++ main.cpp
@@ -8,45 +8,53 @@
 
 using namespace std;
 
-typedef map<string, set<string> > Adjlist;
+typedef map<string, set<string>> Adjlist;
 // MATH
 
-int get_N(Adjlist adjlist) {
+int get_N(Adjlist adjlist)
+{
     return adjlist.size();
 }
 
-int get_E(Adjlist adjlist) {
+int get_E(Adjlist adjlist)
+{
     int sum = 0;
-    for (auto const& x : adjlist) {
+    for (auto const &x : adjlist)
+    {
         sum += x.second.size();
     }
-    return sum/2;
+    return sum / 2;
 }
 
-int get_k(int N, int E) {
-    return 2*E/N;
+int get_k(int N, int E)
+{
+    return 2 * E / N;
 }
 
-float get_delta(int N, int E) {
-    return 2*E/(float)(N*(N-1));
+float get_delta(int N, int E)
+{
+    return 2 * E / (float)(N * (N - 1));
 }
 
 // HELPERS
-void print_network_characteristics(Adjlist const &adjlist) 
+void print_network_characteristics(Adjlist const &adjlist)
 {
     int N = get_N(adjlist);
     int E = get_E(adjlist);
     int k = get_k(N, E);
     float delta = get_delta(N, E);
 
-    cout << "N:" << N << " E: " << E << " <k>: " << k << " delta: " << delta << endl; 
+    cout << "N:" << N << " E: " << E << " <k>: " << k << " delta: " << delta << endl;
 }
 
-void print_adjlist(Adjlist adjlist) {
-    for (auto const& x : adjlist) {
+void print_adjlist(Adjlist adjlist)
+{
+    for (auto const &x : adjlist)
+    {
         cout << x.first << ": ";
 
-        for(auto const& y : x.second) {
+        for (auto const &y : x.second)
+        {
             cout << y << " ";
         }
 
@@ -54,56 +62,65 @@ void print_adjlist(Adjlist adjlist) {
     }
 }
 
-bool add_string_value(Adjlist& adjlist, string word_a, string word_b) {
+bool add_string_value(Adjlist &adjlist, string word_a, string word_b)
+{
 
-    if(adjlist.find(word_a) == adjlist.end()) {
+    if (adjlist.find(word_a) == adjlist.end())
+    {
         adjlist[word_a] = set<string>();
     }
 
-    if(word_a == word_b) return false;
+    if (word_a == word_b)
+        return false;
 
     auto const &ret = adjlist[word_a].insert(word_b);
 
     return ret.second;
 }
 
-bool add_edge(Adjlist& adjlist, string a, string b) {
+bool add_edge(Adjlist &adjlist, string a, string b)
+{
     add_string_value(adjlist, a, b);
-    return add_string_value(adjlist, b, a);   
+    return add_string_value(adjlist, b, a);
 }
 
-bool exists_edge(Adjlist& adjlist, string a, string b) {
+bool exists_edge(Adjlist const &adjlist, string a, string b)
+{
     auto const &x = adjlist.find(a);
-    if(x == adjlist.end()) {
+    if (x == adjlist.end())
+    {
         return false;
     }
-    
-   if(x->second.find(b) == x->second.end()) {
-       return false;
-   }
-   
-   return true;
+
+    if (x->second.find(b) == x->second.end())
+    {
+        return false;
+    }
+
+    return true;
 }
 
-vector<string> get_node_names(Adjlist adjlist) {
+vector<string> get_node_names(Adjlist adjlist)
+{
     vector<string> names;
-    for (auto const& x : adjlist) {
+    for (auto const &x : adjlist)
+    {
         names.push_back(x.first);
     }
     return names;
 }
 
-
-
 // PREPROCESS
-Adjlist read_language(string language) {
+Adjlist read_language(string language)
+{
     // read the input
     Adjlist adjlist;
 
     ifstream infile("dependency_networks/" + language);
 
     string a, b;
-    while (infile >> a >> b) {
+    while (infile >> a >> b)
+    {
         add_edge(adjlist, a, b);
     }
 
@@ -141,7 +158,7 @@ bool is_edge_correct(Edge const &a)
     return a.first > a.second;
 }
 
-void exchange_ends(Edge &a, Edge &b) 
+void exchange_ends(Edge &a, Edge &b)
 {
     string tmp = a.second;
     a.second = b.second;
@@ -151,9 +168,9 @@ void exchange_ends(Edge &a, Edge &b)
 void update_edges(Edge &a, Edge &b)
 {
     exchange_ends(a, b);
-    if(not(is_edge_correct(a)))
+    if (not(is_edge_correct(a)))
         swap_edge(a);
-    if(not(is_edge_correct(b)))
+    if (not(is_edge_correct(b)))
         swap_edge(b);
 }
 
@@ -188,12 +205,14 @@ bool is_allowed(Adjlist const &network, Edge const &a, Edge const &b)
         return false;
     else if (a.second == b.first or a.second == b.second)
         return false;
+    else if (exists_edge(network, a.first, b.second) or exists_edge(network, b.first, a.second))
+        return false;
     else
         return true;
 }
 
-
-void update_network(Adjlist &network, Edge const &a, Edge const &b) {
+void update_network(Adjlist &network, Edge const &a, Edge const &b)
+{
 
     network[a.first].erase(a.second);
     network[a.second].erase(a.first);
@@ -213,7 +232,7 @@ Adjlist generate_switching_model(Adjlist const &network, unsigned int Q)
     Adjlist switching_model = Adjlist(network);
     vector<Edge> edges = generate_edge_set(switching_model);
     unsigned int E = edges.size();
-    for (int i = 0; i < Q*E; i++)
+    for (int i = 0; i < Q * E; i++)
     {
         Edge &a = *get_random_edge(edges);
         Edge &b = *get_random_edge(edges);
@@ -222,53 +241,44 @@ Adjlist generate_switching_model(Adjlist const &network, unsigned int Q)
         {
             update_network(switching_model, a, b);
             update_edges(a, b);
-
         }
     }
     return switching_model;
 }
 // GRAPH GENERATION
 
-Adjlist generate_erdos_renyi(vector<string> names, int N, int E) {
+Adjlist generate_erdos_renyi(vector<string> names, int N, int E)
+{
     Adjlist adjlist;
 
     vector<string> aux;
 
     int i = 0;
-    while(i < E) {
+    while (i < E)
+    {
         int a = rand() % N;
         int b = rand() % N;
 
-        if(add_edge(adjlist, names[a], names[b])) {
+        if (add_edge(adjlist, names[a], names[b]))
+        {
             i++;
         }
     }
-
     return adjlist;
 }
 
 // MAIN
 
-void process_language(string language) {
+void process_language(string language)
+{
     Adjlist adjlist = read_language(language);
-
-    int N = get_N(adjlist);
-    int E = get_E(adjlist);
-    int k = get_k(N, E);
-    float delta = get_delta(N, E);
-
-    cout << "N:" << N << " E: " << E << " <k>: " << k << " delta: " << delta << endl; 
-
-    // compute......
-    cout << exists_edge(adjlist, ",", "azaldu") << endl;
-    cout << exists_edge(adjlist, ",", "xxxxx") << endl;
-
-    Adjlist random_graph = generate_erdos_renyi(get_node_names(adjlist), N, E);
-
-    print_adjlist(random_graph);
+    Adjlist switching_model = generate_switching_model(adjlist, 5);
+    print_network_characteristics(adjlist);
+    print_network_characteristics(switching_model);
 }
 
-int main() {
+int main()
+{
     string language = "Basque_syntactic_dependency_network.txt";
 
     cout << "Lenguage: " << language << endl;
